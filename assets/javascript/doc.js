72fca559eb7841551b4227a538279f5d384b58f2
Nicholas Mahase <nicholasmahase@gmail.com>
2018-11-14 21:32:09 -0500

    moment

--- assets/javascript/doc.js
+++ assets/javascript/doc.js
@@ -41,17 +41,22 @@ var config = {
 
          // The createRow function takes data returned form and appends the table data to the tbody
  database.ref("formInfo").on("child_added",function(childSnapshot) {
+     var now = moment();
+     var prevDate = moment(childSnapshot.val().date);
     var employeeData = "<tr>";
     employeeData += "<td>" + childSnapshot.val().name + "</td>";
     employeeData += "<td>" + childSnapshot.val().role + "</td>";
     employeeData += "<td>" + childSnapshot.val().date + "</td>";
-    employeeData += "<td></td>";
+    employeeData += "<td>" + now.diff(prevDate, 'months') +"</td>";
     employeeData += "<td>" + childSnapshot.val().rate + "</td>";
     employeeData += "<td></td>";
     employeeData += "</tr>";
 
     $('tbody').append(employeeData);
  });
+
+
+
     
         
 
